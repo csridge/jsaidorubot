1606efda01a6fefe95bcde63709c68972b2d9eeb
John Ridge <129057739+csridge@users.noreply.github.com>
2025-06-06 17:41:11 +0700

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,25 +1,28 @@
 import { Chess } from 'https://esm.sh/chess.js';
 import { evaluate } from './evaluate.js';
 
-const game = new Chess()
-
-export function findBestMove(board) {
-  const moves = game.moves(); // all legal moves
+export function findBestMove(game) {
+  const moves = game.moves();
   let best_move = null;
-  let best_eval = -Infinity
-  for (let move of moves){
-    board.move(move)
-    let evaluation = evaluate(board)
-    board.undo()
-    if (best_eval === null || evaluation > best_eval){
-        best_eval = evaluation
-        best_move = move
+  let best_eval = -Infinity;
+  for (let move of moves) {
+    game.move(move);
+    let evaluation = evaluate(game);
+    game.undo();
+    if (evaluation > best_eval) {
+      best_eval = evaluation;
+      best_move = move;
     }
   }
-  return best_move
+  return best_move;
 }
 
-export function makeMove(move){
-    game.move(findBestMove(game))
+export function makeMove(game, board) {
+  const move = findBestMove(game);
+  if (move) {
+    game.move(move);
     board.position(game.fen());
+    document.querySelector('#fen').innerText = game.fen();
+    document.querySelector('#pgn').innerText = game.pgn();
+  }
 }
